6f2b0276e7cc9dde0fcdf8fd921d978476d2ee48
Goutamyadav911 <160489498+Goutamyadav911@users.noreply.github.com>
2024-04-28 12:58:18 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+
+function openSidebar() {
+  if (!sidebarOpen) {
+    sidebar.classList.add('sidebar-responsive');
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if (sidebarOpen) {
+    sidebar.classList.remove('sidebar-responsive');
+    sidebarOpen = false;
+  }
+}
